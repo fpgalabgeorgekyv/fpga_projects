bc54a8f92dd578399e10d68e413cd6f1d93d80bf
fpgalabgeorgekyv <148938656+fpgalabgeorgekyv@users.noreply.github.com>
2023-12-13 12:05:29 +0200

    Update hardwiredlib.vhd

--- Lab4/hardwiredlib.vhd
+++ Lab4/hardwiredlib.vhd
@@ -3,21 +3,21 @@ use ieee.std_logic_1164.all;
 
 package mseqlib is
 
-component mux4 is
-generic (n:integer:=8);
-port(dina,dinb : in std_logic_vector (n-1 downto 0);
-	dinc,dind : in std_logic_vector (n-1 downto 0);
-	sel : in std_logic_vector(1 downto 0);
-	dout : out std_logic_vector (n-1 downto 0));
+component decoder4bit is
+port( din            	  : in std_logic_vector(3 downto 0);
+           dout             : out std_logic_vector(15 downto 0));
 end component;
 
-component regnbit is
-generic (n: integer :=8);
-	port( din : in std_logic_vector(n-1 downto 0);
-		clk,rst,ld : in std_logic;
-		inc : in std_logic;
-		dout : out std_logic_vector(n-1 downto 0));
+component decoder3bit is
+port( din            	  : in std_logic_vector(3 downto 0);
+           dout             : out std_logic_vector(15 downto 0));
 end component;
 
-
+component counter is
+    Port ( clk : in STD_LOGIC;
+           rst : in STD_LOGIC;
+           incr : in STD_LOGIC;
+           count : out STD_LOGIC_VECTOR(3 downto 0));
+end component;
+	
 end package mseqlib;
